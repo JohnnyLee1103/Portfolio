d9ab23b877022bc370bca329c2125e0c1a72fd7e
JohnnyLee1103 <139614575+JohnnyLee1103@users.noreply.github.com>
2023-11-01 04:02:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -36,15 +36,6 @@ function hideImage(imageId) {
   }
 }
 
-window.addEventListener('mousemove', function(e){
-  var bg = document.getElementById("bg-move");
-  var bgX = e.clientX;
-  var bgY = e.clientY;
-
-  bg.style.backgroundPositionX = (bgX * 0.02) + '%';
-  bg.style.backgroundPositionY = (bgY * 0.02) + '%';
-})
-
 const scrollDownLink = document.getElementById('scroll-down-link');
 
 scrollDownLink.addEventListener('click', function (e) {
